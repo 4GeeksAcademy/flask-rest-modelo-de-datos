3afb9e13db420badc5fb4fa82592b30dce56f7d4
Génesis Falcón <genesisfalcon15@gmail.com>
2026-03-20 22:35:34 +0000

    OK

--- src/models.py
+++ src/models.py
@@ -1,19 +1,65 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+class User(db.Model):
+    __tablename__ = 'user'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(
+        String(50), unique=True, nullable=False)
+    firstname: Mapped[str] = mapped_column(String(50), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(100), nullable=False)
+    is_active: Mapped[bool] = mapped_column(nullable=False, default=True)
+
+    posts = relationship("Post", back_populates="user")
+    comments = relationship("Comment", back_populates="user")
 
     def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+        return {"id": self.id, "username": self.username}
+
+
+class Post(db.Model):
+    __tablename__ = 'post'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+
+    user = relationship("User", back_populates="posts")
+    media = relationship("Media", back_populates="post")
+    comments = relationship("Comment", back_populates="post")
+
+    def serialize(self):
+        return {"id": self.id}
+
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(String(255), nullable=False)
+    author_id: Mapped[int] = mapped_column(
+        ForeignKey('user.id'), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'), nullable=False)
+
+    user = relationship("User", back_populates="comments")
+    post = relationship("Post", back_populates="comments")
+
+    def serialize(self):
+        return {"id": self.id, "text": self.comment_text}
+
+
+class Media(db.Model):
+    __tablename__ = 'media'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[str] = mapped_column(String(50), nullable=False)
+    url: Mapped[str] = mapped_column(String(255), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'), nullable=False)
+
+    post = relationship("Post", back_populates="media")
+
+    def serialize(self):
+        return {"id": self.id, "url": self.url}
